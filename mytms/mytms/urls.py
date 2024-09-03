58ae5f609ffec613bfe455e2bc90a5f812fbefda
Marcelo Feliciano Filho <marcelo.feliciano@salessense.com.br>
2024-09-02 21:43:15 -0300

    Created the urls with routers.

--- mytms/mytms/urls.py
+++ mytms/mytms/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from ..tms_manager.views import CampaignViewSet, MemberViewSet, TaskViewSet
+
+router = DefaultRouter()
+router.register(r"campaigns", CampaignViewSet)
+router.register(r"members", MemberViewSet)
+router.register(r"tasks", TaskViewSet)
 
 urlpatterns = [
     path("tms_manager/admin/", admin.site.urls),
+    path("/api", include(router.urls))
 ]
